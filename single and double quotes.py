2e49f79f8c8b9fb0366bd1849d2f484dd88530ae
Dinesh A <dineshkavitha2005@gmail.com>
2026-05-04 21:18:41 +0530

    Add files via upload

---
+++ single and double quotes.py
@@ -0,0 +1,9 @@
+#not executed because it terminated at first single quotes
+message='bobby's home'
+print(message)
+#instead use this method
+message='bobby\'s home'
+print(message)
+#or this method
+message="bobby's home"
+print(message)
