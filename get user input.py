74767f9703cd3d66d524c1526cb1507c5fbcb4f4
Dinesh A <dineshkavitha2005@gmail.com>
2026-06-04 19:09:40 +0530

    Add files via upload

---
+++ get user input.py
@@ -0,0 +1,12 @@
+a=input()
+b=input()
+c=a+b
+print(c)
+# THE VALUE HAS BEEN STORED AS STRING TYPE AND RESULTED AS 1020
+
+a=int(input())
+b=int(input())
+c=a+b
+print(c)
+
+# TO RESOLVE THIS , USE CASTING METHOD TO SPECIFY THE PARTICULAR DATATYPE
