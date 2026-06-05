543d5c88576391b1116b048b37cf9bd00e82a9c9
Dinesh A <dineshkavitha2005@gmail.com>
2026-06-05 14:38:40 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,13 @@
+if(True):
+    print("yes")
+else:
+    print("no")
+# TRUE - IF STATEMENT EXECUTE
+# FALSE - ELSE STATEMENT EXECUTE
+# FIRST LETTER IN TRUE AND FALSE SHOULD BE IN CAPITAL
+
+if(False):
+    print("yes")
+else:
+    print("no")
+
