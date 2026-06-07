b8c8706bfee6c6a9ee16a54b515313fcb64c433e
Dinesh A <dineshkavitha2005@gmail.com>
2026-06-07 18:35:01 +0530

    Add files via upload

---
+++ question3.py
@@ -0,0 +1,9 @@
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=int(input("Enter 3rd number:"))
+mul=a*b*c
+add=a+b+c
+div=mul/add
+print("multiplication:",mul)
+print("addition:",add)
+print("division:",div)
