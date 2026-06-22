a19abcf50f8a67d3d21b5f783ea0992c38c48633
Dinesh A <dineshkavitha2005@gmail.com>
2026-06-22 17:17:54 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,8 @@
+for i in range(5):
+    print("*")
+# IT PRINTS * IN NEXT NEXT LINE
+
+for i in range(5):
+    print("*",end="")
+# TO PRINT * IN SINGLE LINE
+# WHATEVER INSIDE THE END WILL PRINT IN BETWEEN THE *
