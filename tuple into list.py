6864c7c48a5964549e8df4d68effa30da1925a61
Dinesh A <dineshkavitha2005@gmail.com>
2026-07-17 19:10:36 +0530

    Add files via upload

---
+++ tuple into list.py
@@ -0,0 +1,12 @@
+a=(1,2,3,4)
+b=list(a)
+print(b)
+# TUPLE CAN'T BE MODIFY
+# INSTEAD WE CAN CAST TUPLE INTO LIST BY USING b=list(a)
+
+a=(1,2,3,4)
+b=list(a)
+b.pop()
+print(b)
+
+# AFTER THAT WE CAN DONE ALL OPERATIONS LIKE LIST 
