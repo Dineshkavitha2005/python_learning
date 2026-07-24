654652a0a627ea13f3f2b40dce52eda3ec1d088d
Dinesh A <dineshkavitha2005@gmail.com>
2026-07-24 18:44:59 +0530

    Add files via upload

---
+++ delete dictionary.py
@@ -0,0 +1,31 @@
+a={"name":"kavi",
+   "age":45,
+   "location":"chennai",
+   "students":{"karthi","siva"}
+    }
+a.pop("students")
+print(a)
+
+
+
+# OR
+
+a={"name":"kavi",
+   "age":45,
+   "location":"chennai",
+   "students":{"karthi","siva"}
+    }
+del a["age"]
+print(a)
+
+
+# OR
+
+a={"name":"kavi",
+   "age":45,
+   "location":"chennai",
+   "students":{"karthi","siva"}
+    }
+del a
+print(a)
+
